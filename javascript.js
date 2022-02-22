44c197b2fbfb1dd249650500c7a46d028474a32b
Jose <sesejose@gmail.com>
2022-02-22 11:42:57 +0100

    Nav-Mobile-Desktop

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// Open search field mobile
+
+function openSearch() {
+  var x = document.getElementById("search");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// Open Main Menu Mobile
+function openMainMenu() {
+  var x = document.getElementById("mainmenu");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
